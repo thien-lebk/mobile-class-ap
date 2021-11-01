b64c0eb592d0c1c108a5c9afca82de2f74d833f6
huunghianguyen99 <nguyenhuunghia99@gmail.com>
2021-11-01 22:37:47 +0700

    Add TabBar

--- lib/social.dart
+++ lib/social.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:phinder/chat.dart';
+import 'package:phinder/exercise.dart';
 
 class Social extends StatefulWidget {
   const Social({Key? key}) : super(key: key);
@@ -10,26 +12,53 @@ class Social extends StatefulWidget {
 class _SocialState extends State<Social> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-            child: Column(
-          children: [
-            Text('Trang chính'),
-            SizedBox(height: 20),
-            InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, '/chat');
-                },
-                child: Text('Nhấn vô để làm trang chat')),
-            SizedBox(height: 20),
-            InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, '/exercise');
-                },
-                child: Text('Nhấn vô để làm trang exercise'))
-          ],
-        )),
+    return DefaultTabController(
+      length: 2,
+      child: SafeArea(
+        child: Scaffold(
+          body:
+              // Container(
+              //   child: Column(
+              //     children: [
+              //       Text('Trang chính'),
+              //       SizedBox(height: 20),
+              //       InkWell(
+              //           onTap: () {
+              //             Navigator.pushNamed(context, '/chat');
+              //           },
+              //           child: Text('Nhấn vô để làm trang chat')),
+              //       SizedBox(height: 20),
+              //       InkWell(
+              //           onTap: () {
+              //             Navigator.pushNamed(context, '/exercise');
+              //           },
+              //           child: Text('Nhấn vô để làm trang exercise'))
+              //     ],
+              //   ),
+              // ),
+              TabBarView(
+            children: [
+              ExerciseScreen(),
+              ChatScreen(),
+            ],
+          ),
+          bottomNavigationBar: TabBar(
+            tabs: [
+              Tab(
+                child: Text(
+                  'Exercise',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'Chat',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
